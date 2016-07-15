5507a7266809c750303dbdea1a597dce3b5582a8
jessalbarian <albarian.jess@gmail.com>
2016-07-15 15:59:37 -0600

    trying to get firebase to work

--- getPinData.py
+++ getPinData.py
@@ -2,34 +2,69 @@
 
 import RPi.GPIO as GPIO
 import time
+from firebase import firebase
+from datetime import date
 
+
+
+# Set up Firebase
+firebase = firebase.FirebaseApplication('https://tiltsensorarduino.firebaseio.com', None)
+
+# Get number of total beers
+result = firebase.get('/beers', None)
+total_beers = result['total_beers']
+
+# Get today's date and find JSON section based on beerinfo + #
+# If beerinfo1 was on 07/15/16, that is day 1
+
+today = time.strftime("%m/%d/%y")
+first_day = result['beerinfo1']['date']
+
+a = date(int(today[6:8]), int(today[0:2:]), int(today[3:5]))
+b = date(int(first_day[6:8]), int(first_day[0:2]), int(first_day[3:5]))
+days_in_between =(a - b).days
+#i = str(days_in_between + 1)
+i='2'
+if result['beerinfo'+i] is None:
+    print("beerinfo none")
+previous_date = result['beerinfo'+i]['date']
+
+
+# Set up GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 
-beer_count = 0
+
 current_state = 0
 previous_state = 0
-start = 0
 counter = 0
-beerNum = 0
+
+#Get daily beer number
+dailyBeerNum = result['beerinfo'+i]['beersPerDay']
+
+sensor1_pin = 18
+if sensor1_pin == 18:
+    name = "IPA"
+firebase.post('beers', 'beerinfo'+i, {'name':name})
+firebase.post('beers', 'beerinfo'+i, {'date':today})
+firebase.post('beers', 'beerinfo'+i, {'id':i})
 
 while True:
-    current_state = GPIO.input(18)
-
-    if current_state == 1:
+    sensor1_state = GPIO.input(sensor1_pin)
+    
+    if sensor1_state == 1:
         print("Tilt sensor value is up")
 	time.sleep(1)
-	previous_state = current_state
+	previous_state = sensor1_state
     else:
         print("Tilt sensor value is down")
-	if previous_state == current_state:	
+	if previous_state == sensor1_state:	
 	    counter = counter + 1
-	previous_state = current_state
+	previous_state = sensor1_state
 	time.sleep(1)
     if counter == 6:
 	print("-------------------------")
 	print("One beer has been poured!")
-	print("")
 	print("         _.._..,_,_")
 	print("        (          )")
 	print("         ]~,'-\.~~[")
@@ -38,7 +73,12 @@ while True:
 	print("       '=]): \.) ([")
 	print("         |:: '    |")
 	print("          ~~----~")
+	print("-------------------------")
+
 	counter = 0
-	beerNum = beerNum + 1
+	dailyBeerNum = dailyBeerNum + 1
+	total_beers = total_beers + 1
+	firebase.post('beers', 'beerinfo'+i, {'beersPerDay': dailyBeerNum})
+	firebase.post('beers', 'total_beers', total_beers)
 
 GPIO.cleanup()
