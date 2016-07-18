1ef5c73868cef1aa83fb2803a2c726ad67b90ff5
jessalbarian <albarian.jess@gmail.com>
2016-07-18 14:41:55 -0600

    updated firebase

--- getPinData.py
+++ getPinData.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 import time
 from firebase import firebase
 from datetime import date
-
+import datetime
 
 
 # Set up Firebase
@@ -13,25 +13,23 @@ firebase = firebase.FirebaseApplication('https://tiltsensorarduino.firebaseio.co
 # Get number of total beers
 result = firebase.get('/beers', None)
 total_beers = result['total_beers']
+sensor1 = result['Sensor1']
+sensor2 = result['Sensor2']
+
 
 # Get today's date and find JSON section based on beerinfo + #
 # If beerinfo1 was on 07/15/16, that is day 1
-
 today = time.strftime("%m/%d/%y")
-first_day = result['beerinfo1']['date']
+# Get date formatted a certain way
+thedate = datetime.date.today()
+first_day = result['Sensor1']['beerinfo1']['date']
+print(first_day)
 
 a = date(int(today[6:8]), int(today[0:2]), int(today[3:5]))
 b = date(int(first_day[6:8]), int(first_day[0:2]), int(first_day[3:5]))
 days_in_between =(a - b).days
 
-i = str(days_in_between + 1)
-print("a: %s" % a)
-print("b: %s" % b)
-print("i: %s" % i)
-#i='2'
-#if result['beerinfo'+i] is None:
- #   print("beerinfo none")
-#previous_date = result['beerinfo'+i]['date']
+i = str(days_in_between)
 
 # Set up GPIO
 GPIO.setmode(GPIO.BCM)
@@ -44,18 +42,17 @@ counter = 0
 
 
 sensor1_pin = 18
+
+# Set name of beer
 if sensor1_pin == 18:
-    name = "IPA"
-#firebase.put('beers', ('beerinfo'+i), {'name':name, 'date':today, 'id':i})
-#firebase.post('/beers', 'beerinfo'+i: {'date':today})
-#firebase.post('/beers', 'beerinfo'+i: {'id':i})
+    name = "India Pale Ale"
+
+
 try:
-    dailyBeerNum = result['beerinfo'+i]['beersPerDay']
-    firebase.put('beers', 'beerinfo'+i, {'name':name, 'date':today, 'id':int(i), 'beersPerDay': dailyBeerNum}
-   # dailyBeerNum = result['beerinfo'+i]['beersPerDay']
-)
+    dailyBeerNum = result['Sensor1']['beerinfo'+i]['beersPerDay']
+    firebase.put('beers/Sensor1', 'beerinfo'+i, {'datetime':thedate, 'name':name, 'date':today, 'id':int(i), 'beersPerDay': dailyBeerNum})
 except Exception as Error:
-    firebase.put('beers', ('beerinfo'+i), {'name':name, 'date':today, 'id':int(i), 'beersPerDay': 0})
+    firebase.put('beers/Sensor1', ('beerinfo'+i), {'datetime':thedate, 'name':name, 'date':today, 'id':int(i), 'beersPerDay': 0})
     dailyBeerNum = 0
 
 while True:
@@ -87,7 +84,7 @@ while True:
 	counter = 0
 	dailyBeerNum = dailyBeerNum + 1
 	total_beers = total_beers + 1
-	firebase.put('beers', 'beerinfo'+i, {'beersPerDay': dailyBeerNum, 'name':name, 'date':today, 'id':i})
+	firebase.put('beers/Sensor1', 'beerinfo'+i, {'datetime': thedate, 'beersPerDay': dailyBeerNum, 'name':name, 'date':today, 'id':i})
 	firebase.put('beers', 'total_beers', total_beers)
 
 GPIO.cleanup()
