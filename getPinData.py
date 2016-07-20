e8a3a2d97f5bfde873aa764aeb984fb57d6032c6
jessalbarian <albarian.jess@gmail.com>
2016-07-20 10:29:51 -0600

    updated getpindata to include time of beer

--- getPinData.py
+++ getPinData.py
@@ -12,13 +12,13 @@ import datetime
 # Set up Firebase
 #----------------
 firebase = firebase.FirebaseApplication('https://tiltsensorarduino.firebaseio.com', None)
+result = firebase.get('/beers', None)
 
 
 
 #--------------------------
 # Get number of total beers
 #--------------------------
-result = firebase.get('/beers', None)
 total_beers = result['total_beers']
 
 
@@ -27,7 +27,6 @@ total_beers = result['total_beers']
 # Set up date
 #------------
 today = time.strftime("%m/%d/%y")
-beertime = time.strftime("%H:%M:%S")
 
 
 # Get date formatted a specific way
@@ -37,7 +36,10 @@ thedate = datetime.date.today()
 first_day = result['Sensor1']['beerinfo1']['date']
 first_dayS2 = result['Sensor2']['beerinfo1']['date']
 
+
+#-----------------------------------------------
 # Calculate days in between to get value i and j
+#-----------------------------------------------
 a = date(int(today[6:8]), int(today[0:2]), int(today[3:5]))
 
 b1 = date(int(first_day[6:8]), int(first_day[0:2]), int(first_day[3:5]))
@@ -50,7 +52,9 @@ if days_in_betweenS2 == 1:
 if days_in_betweenS1 == 1:
     days_in_betweenS1 = days_in_betweenS1 + 1
 i = str(days_in_betweenS1)
-j = str(days_in_betweenS2)
+j = str(days_in_betweenS2+1)
+print("i " +i)
+print("j " +j)
 
 
 #------------
@@ -73,27 +77,53 @@ counter = 0
 counterS2 = 0
 sensor1_pin = 12
 sensor2_pin = 18
+beertimeArrayS1 = []
+beertimeArrayS2 = []
+
+"""
+for k in range(len(result['Sensor1']['beerinfo'+i]['beertimes'])):
+    beertimeArrayS1.append(result['Sensor1']['beerinfo'+i]['beertimes']['beer'+str(k+1)])
+    k = beertimeArrayS1[len(result['Sensor1']['beerinfo'+i]['beertimes'])-1]
+    print(k)
+for l in range(len(result['Sensor2']['beerinfo'+i]['beertimes'])):
+    beertimeArrayS2.append(result['Sensor2']['beerinfo'+i]['beertimes']['beer'+str(l+1)])
+print(beertimeArrayS1)
+print(beertimeArrayS2)
+"""
 
 
 #-------------------------------
 # Create new objects to database
 #-------------------------------
+# Sensor1 Objects
+
 try:
     dailyIPANum = result['Sensor1']['beerinfo'+i]['beersPerDay']
-    firebase.put('beers/Sensor1', 'beerinfo'+i, {'datetime':thedate, 'name': 'India Pale Ale', 'date':today, 'id':int(i), 'beersPerDay': dailyIPANum, 'beertimes' :{'beer'+i:beertime}})
+    for m in range(len(result['Sensor1']['beerinfo'+i]['beertimes'])):
+        beertimeArrayS1.append(result['Sensor1']['beerinfo'+i]['beertimes']['beer'+str(m)])
+        k = beertimeArrayS1[len(result['Sensor1']['beerinfo'+i]['beertimes'])]
+        print(k)
+    firebase.put('beers/Sensor1', 'beerinfo'+i, {'datetime':thedate, 'name': 'India Pale Ale', 'date':today, 'id':int(i), 'beersPerDay': dailyIPANum})
 
 except Exception as Error:
-    firebase.put('beers/Sensor1', ('beerinfo'+i), {'datetime':thedate, 'name':'India Pale Ale', 'date':today, 'id':int(i), 'beersPerDay': 0, 'beertimes' :{'beer'+i:beertime}})
+    k = 1
+    firebase.put('beers/Sensor1', ('beerinfo'+i), {'datetime':thedate, 'name':'India Pale Ale', 'date':today, 'id':int(i), 'beersPerDay': 0})
     dailyIPANum = 0
 
 
+# Sensor2 Objects
 try:
     dailyPorterNum = result['Sensor2']['beerinfo'+j]['beersPerDay']
-    firebase.put('beers/Sensor2', 'beerinfo'+j, {'datetime':thedate, 'name': 'Chocolate Porter', 'date':today, 'id': int(j), 'beersPerDay': dailyPorterNum, 'beertimes' : {'beer'+i:beertime}})
+    for l in range(len(result['Sensor2']['beerinfo'+i]['beertimes'])):
+        beertimeArrayS2.append(result['Sensor2']['beerinfo'+i]['beertimes']['beer'+str(l)])
+        n = beertimeArrayS1[len(result['Sensor1']['beerinfo'+i]['beertimes'])]
+        print(n)
+    firebase.put('beers/Sensor2', 'beerinfo'+j, {'datetime':thedate, 'name': 'Chocolate Porter', 'date':today, 'id': int(j), 'beersPerDay': dailyPorterNum})
 
 except Exception as Error:
-    firebase.put('beers/Sensor2', ('beerinfo'+j), {'datetime':thedate, 'name':'Chocolate Porter', 'date':today, 'id':int(j), 'beersPerDay': 0, 'beertimes' :{'beer'+i:beertime}})
+    firebase.put('beers/Sensor2', ('beerinfo'+j), {'datetime':thedate, 'name':'Chocolate Porter', 'date':today, 'id':int(j), 'beersPerDay': 0})
     dailyPorterNum = 0
+    n = 1
 
 #----------------
 # Loop for sensor
@@ -131,7 +161,8 @@ while True:
 	counter = 0
 	dailyIPANum = dailyIPANum + 1
 	total_beers = total_beers + 1
-
+        beertimeS1 = time.strftime("%H:%M:%S")        
+        k = k + 1
     if counterS2 == 3:
 	print("----------------------")
 	print("Sensor2 poured a beer!")
@@ -139,10 +170,11 @@ while True:
 	counterS2 = 0
 	dailyPorterNum = dailyPorterNum + 1
 	total_beers = total_beers + 1
-
-	firebase.put('beers/Sensor1', 'beerinfo'+i, {'datetime': thedate, 'beersPerDay': dailyIPANum, 'name':'India Pale Ale', 'date':today, 'id':i, 'beertimes' :{'beer'+i:beertime}})
-	firebase.put('beers/Sensor2', 'beerinfo'+j, {'datetime': thedate, 'beersPerDay': dailyPorterNum, 'name':'Chocolate Porter', 'date':today, 'id':j, 'beertimes' :{'beer'+i:beertime}})
-	firebase.put('beers', 'total_beers', total_beers)
+        beertimeS2 = time.strftime("%H:%M:%S")
+        n = n + 1
+        firebase.put('beers/Sensor1', 'beerinfo'+i, {'datetime': thedate, 'beersPerDay': dailyIPANum, 'name':'India Pale Ale', 'date':today, 'id':i, 'beertimes' :{'beer'+str(k):beertimeS1}})
+        firebase.put('beers/Sensor2', 'beerinfo'+j, {'datetime': thedate, 'beersPerDay': dailyPorterNum, 'name':'Chocolate Porter', 'date':today, 'id':j, 'beertimes' :{'beer'+str(n):beertimeS2}})
+        firebase.put('beers', 'total_beers', total_beers)
 
 
 GPIO.cleanup()
