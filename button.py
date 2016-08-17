1337eb52dba2439ab933a37636178a230e25bcde
jessalbarian <albarian.jess@gmail.com>
2016-08-17 17:14:07 -0600

    Revert "meow"
    This reverts commit 29adea675a005572945b8894d153648f22f0d73f.

--- button.py
+++ button.py
@@ -7,6 +7,15 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3,GPIO.IN)
 buttonPin = 3
 
+prev_input = 0
+'''
+while True:
+  input = GPIO.input(3)
+  if ((not prev_input) and input):
+    print("Button pressed")
+  prev_input = input
+  time.sleep(0.05)
+'''
 while True:
   #assuming the script to call is long enough we can ignore bouncing
   if (GPIO.input(buttonPin)):
